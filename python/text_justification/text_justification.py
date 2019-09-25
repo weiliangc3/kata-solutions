2fd4d488836fde0e76cad996f2fe56321d0fc034
Wei <weiliang.c3@gmail.com>
2019-09-25 21:46:24 +0100

    fix the final line justification

--- python/text_justification/text_justification.py
+++ python/text_justification/text_justification.py
@@ -7,18 +7,20 @@ def justify_text(input, expected_width):
     for i, word in enumerate(words):
         word_length = len(word)
         if (line_char_count + word_length > expected_width):
-            justified_text = resolve_stack(word_stack, justified_text, expected_width)
+            justified_text = process_word_stack(word_stack, justified_text, expected_width)
             justified_text += "\n"
             line_char_count = 0
             word_stack.clear()
         word_stack.append(word)
         line_char_count += (word_length + 1)
         if (i == len(words) - 1):
-            justified_text = resolve_stack(word_stack, justified_text, expected_width)
+            final_word_stack = [i + " " for i in word_stack]
+            final_word_stack[len(final_word_stack) - 1] = final_word_stack[len(final_word_stack) - 1].strip()
+            justified_text = add_stack_to_text(final_word_stack, justified_text)
 
     return justified_text
 
-def resolve_stack(word_stack, justified_text, expected_width):
+def process_word_stack(word_stack, justified_text, expected_width):
     current_index = 0
     while get_line_length_from_array(word_stack) < expected_width:
         if len(word_stack) == 1:
@@ -28,10 +30,19 @@ def resolve_stack(word_stack, justified_text, expected_width):
         if current_index == len(word_stack)-1:
             current_index = 0
 
-    for word in word_stack:
-        justified_text += word
+    justified_text = add_stack_to_text(word_stack, justified_text)
     
     return justified_text
+
+def add_stack_to_text(word_stack, base_text):
+    for word in word_stack:
+        base_text += word
+    return base_text
+
+def add_single_space_to_words(word_stack):
+    for word in word_stack:
+        word += " "
+    return word_stack
 
 def get_line_length_from_array(line):
     line_length = 0
--- python/text_justification/text_justification_test.py
+++ python/text_justification/text_justification_test.py
@@ -4,12 +4,12 @@ from text_justification import justify_text
 def test_when_given_two_words():
     result = justify_text(input = "hello wei", expected_width = 12)
     print(result)
-    assert result == "hello    wei"
+    assert result == "hello wei"
 
 def test_when_given_a_long_string():
     result = justify_text(input = "hello wei you got this right yes", expected_width = 12)
     print(result)
-    assert result == "hello    wei\nyou got this\nright    yes"
+    assert result == "hello    wei\nyou got this\nright yes"
 
 def test_when_given_variable_widths():
     result = justify_text(input = "hello wei you got this right yes", expected_width = 14)
